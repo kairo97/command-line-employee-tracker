9ebe2a1da7c9ee5c954f2a3cabc3c9aca91e1d06
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-02-01 14:11:24 -0800

    applied middleware and linking to mysql localhost

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const express = require('express');
 const mysql = require('mysql2')
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'HotSauce1997!',
+        database: 'comapny_db'
+    },
+    console.log('connected to company_db.')
+);
